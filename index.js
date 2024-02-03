129ab4e9b3326212d80a2673a4fa34837b0d14e4
spaghetsie <vachvojtech1212@gmail.com>
2024-02-03 21:48:46 +0100

    setup ssl

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { readFile } = require("fs").promises;
 const express = require('express');
 
+var http = require('http');
+var https = require('https');
+var privateKey  = await fs.readFileSync('ssl/serlfsigned.key', 'utf8');
+var certificate = await fs.readFileSync('ssl/serlfsigned.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 const app = express();
 
 console.log()
@@ -10,4 +17,8 @@ app.get("/", async (requrest, response) => {
     response.send(await readFile('./src/main.html', 'utf-8'));
 })
 
-app.listen(process.env.PORT || 80, () => {console.log("tvoje máma frč9")})
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(process.env.PORT || 80, () => {console.log("tvoje máma 80kg")})
+httpsServer.listen(process.env.PORT || 443, () => {console.log("tvoje máma 443kg")})
