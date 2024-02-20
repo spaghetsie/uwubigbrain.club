dc7cba75e7215cfe6d477294b4ad27719a628748
spaghetsie <vachvojtech1212@gmail.com>
2024-02-20 03:35:50 +0100

    added config files and DISCORD INTEGRATED AUTH MY GOD IT TOOK ME 4.5 HOURS ITS NOW 3:30AM NO WONDER DEVS DIE YOUNG

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const engine = require('express-edge');
 
 const http = require('http');
 const https = require('https');
-const islocal = fs.existsSync("./localhost");
+
+require(`${__dirname}/config.js`)
+config.EndpointUriEncoded = encodeURIComponent(config.Endpoint)
 
 const credentials = {key: "privateKey", cert: "certificate"};
 
-if (!islocal){
+if (!config.IsRunningLocally){
 
     const privateKey  = fs.readFileSync('/etc/letsencrypt/live/uwubigbrain.club/privkey.pem', 'utf8');
     const certificate = fs.readFileSync('/etc/letsencrypt/live/uwubigbrain.club/fullchain.pem', 'utf8');
@@ -34,29 +36,76 @@ app.use('/favicon.ico', express.static(`${__dirname}/src/favicon/favicon.ico`));
 //app.enable('view cache');
 
 app.get('/', (request, response) => {
-  response.render('main', { request });
-})
+    response.render('public/main', { request });
+  }
+)
 
 app.get('/epiclegacy', async (request, response) => {
     response.send(await readFile("./public/main.html", "utf-8"))
+  }
+)
+
+app.get("/auth", async (request, response, next) => {
+  
+  if(!request.query.code){
+    await response.redirect(`https://discord.com/api/oauth2/authorize?client_id=1209270223593799800&response_type=code&redirect_uri=${config.EndpointUriEncoded}%2Fauth&scope=identify`)
+    return;
+  }
+
+  const code = request.query.code;
+
+  const auth_request = new Request('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: config.client_id,
+					client_secret: config.client_secret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `${config.Endpoint}/auth`,
+					scope: 'identify',
+				}).toString(),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
+
+  let oauthData;
+  await fetch(auth_request)
+  .then(response => response.json())
+  .then(data => oauthData = data)
+  .catch(error => response.status(401).render('public/error'));
+  
+  let userdata;
+  await fetch('https://discord.com/api/users/@me', {
+    headers: {
+      authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+    },
   })
+  .then(response => response.json())
+  .then(data => userdata = data)
+  .catch(error => response.status(401).render('public/error'));
+
+  response.render('public/main', userdata)
+  
+})
 
 app.use(( req, res, next) => {
-    res.status(404).render('error', { res });
-  })
+    res.status(404).render('public/error',  res );
+  }
+)
 
-if (!islocal) {
+if (!config.IsRunningLocally) {
 
-    var httpServer = http.createServer(app);
-    var httpsServer = https.createServer(credentials, app);
+  var httpServer = http.createServer(app);
+  var httpsServer = https.createServer(credentials, app);
 
-    httpServer.listen(process.env.PORT || 80, () => {console.log("Listening on port 80")})
-    httpsServer.listen(process.env.PORT || 443, () => {console.log("Listening on port 443")})
+  httpServer.listen(process.env.PORT || 80, () => {console.log("Listening on port 80")})
+  httpsServer.listen(process.env.PORT || 443, () => {console.log("Listening on port 443")})
 
 }
 else {
 
-    app.listen(process.env.PORT || 8080, () => {console.log("Running on localhost:8080")})
+  app.listen(process.env.PORT || 8080, () => {console.log("Running on localhost:8080")})
 
 }
 
