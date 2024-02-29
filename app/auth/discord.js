fc33cf7ff4324b5267eb1f8063b666e045e84354
spaghetsie <vachvojtech1212@gmail.com>
2024-02-29 20:03:59 +0100

    config refactor

--- app/auth/discord.js
+++ app/auth/discord.js
@@ -1,89 +1,88 @@
-require('../config/config.js')
-const express = require('express');
+require("../config/config.js");
+const express = require("express");
 
 const DiscordAuthRouter = express.Router();
 
 function CheckForCode(request, response, next) {
-  if (!request.query.code) {
-    response.redirect(`https://discord.com/api/oauth2/authorize?client_id=1209270223593799800&response_type=code&redirect_uri=${config.EndpointUriEncoded}%2Fauth%2Flogin&scope=identify`)
-    return;
-  }
+    if (!request.query.code) {
+        response.redirect(
+            `https://discord.com/api/oauth2/authorize?client_id=1209270223593799800&response_type=code&redirect_uri=${config.server.EndpointUriEncoded}%2Fauth%2Flogin&scope=identify`
+        );
+        return;
+    }
 
-  next();
+    next();
 }
 async function GetToken(request, response, next) {
-  const code = request.query.code;
+    const code = request.query.code;
 
-  const auth_request = new Request('https://discord.com/api/oauth2/token', {
-    method: 'POST',
-    body: new URLSearchParams({
-      client_id: config.client_id,
-      client_secret: config.client_secret,
-      code,
-      grant_type: 'authorization_code',
-      redirect_uri: `${config.Endpoint}/auth/login`,
-      scope: 'identify',
-    }).toString(),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
+    const auth_request = new Request("https://discord.com/api/oauth2/token", {
+        method: "POST",
+        body: new URLSearchParams({
+            client_id: config.discord.client_id,
+            client_secret: config.discord.client_secret,
+            code,
+            grant_type: "authorization_code",
+            redirect_uri: `${config.server.Endpoint}/auth/login`,
+            scope: "identify",
+        }).toString(),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    });
 
-  await fetch(auth_request)
-    .then(response => response.json())
-    .then(data => response.locals.oauthData = data)
-    .catch(error => response.status(401).render('error/error'));
+    await fetch(auth_request)
+        .then((response) => response.json())
+        .then((data) => (response.locals.oauthData = data))
+        .catch((error) => response.status(401).render("error/error"));
 
-  next()
+    next();
 }
 
 async function GetUserData(request, response, next) {
+    await fetch("https://discord.com/api/users/@me", {
+        headers: {
+            authorization: `${response.locals.oauthData.token_type} ${response.locals.oauthData.access_token}`,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => (request.session.user = data))
+        .catch((error) => response.status(401).render("error/error"));
 
-  await fetch('https://discord.com/api/users/@me', {
-    headers: {
-      authorization: `${response.locals.oauthData.token_type} ${response.locals.oauthData.access_token}`,
-    },
-  })
-    .then(response => response.json())
-    .then(data => request.session.user = data)
-    .catch(error => response.status(401).render('error/error'));
-
-  response.redirect(`${config.Endpoint}/`)
+    response.redirect(`${config.server.Endpoint}/`);
 }
 
 async function Logout(request, response, next) {
+    if (!request.session.user) {
+        next();
+        return;
+    }
 
-  if (!request.session.user) {
+    const auth_request = new Request(
+        "https://discord.com/api/oauth2/token/revoke",
+        {
+            method: "POST",
+            body: new URLSearchParams({
+                token: request.session.user.access_token,
+                token_type_hint: "access_token",
+            }).toString(),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }
+    );
+
+    await fetch(auth_request)
+        .then(() => request.session.destroy())
+        .catch((error) => response.status(401).render("error/error"));
+
     next();
-    return;
-  }
-
-  const auth_request = new Request('https://discord.com/api/oauth2/token/revoke', {
-    method: 'POST',
-    body: new URLSearchParams({
-      token: request.session.user.access_token,
-      token_type_hint: 'access_token'
-    }).toString(),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-
-  await fetch(auth_request)
-    .then(() => request.session.destroy())
-    .catch(error => response.status(401).render('error/error'));
-
-  next()
 }
 
-DiscordAuthRouter.use('/login',
-  CheckForCode,
-  GetToken,
-  GetUserData
-)
+DiscordAuthRouter.use("/login", CheckForCode, GetToken, GetUserData);
 
-DiscordAuthRouter.use('/logout', Logout, (request, response) => {
-  response.redirect(`${config.Endpoint}/`)
-})
+DiscordAuthRouter.use("/logout", Logout, (request, response) => {
+    response.redirect(`${config.server.Endpoint}/`);
+});
 
-module.exports = DiscordAuthRouter
+module.exports = DiscordAuthRouter;
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const { readFile } = require("fs").promises;
-const fs = require('fs');
-const express = require('express');
-const engine = require('express-edge');
-const session = require('express-session')
-const hostValidation = require('host-validation')
+const fs = require("fs");
+const express = require("express");
+const engine = require("express-edge");
+const session = require("express-session");
+const hostValidation = require("host-validation");
 
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const https = require("https");
 
 const DiscordAuthRouter = require("./app/auth/discord");
 
-require(`./app/config/config.js`)
-config.EndpointUriEncoded = encodeURIComponent(config.Endpoint)
-
+require(`./app/config/config.js`);
+config.server.EndpointUriEncoded = encodeURIComponent(config.server.Endpoint);
 
 global.app = express();
 const app = global.app;
@@ -20,84 +19,89 @@ const app = global.app;
 // Automatically sets view engine and adds dot notation to app.render
 app.use(engine);
 
-app.use(session({
-  secret: config.session_secret,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+    session({
+        secret: config.server.session_secret,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
-app.use(hostValidation({
-  hosts: ['34.159.41.47',
-    'localhost:8080',
-    'uwubigbrain.club']
-}))
+app.use(
+    hostValidation({
+        hosts: ["34.159.41.47", "localhost:8080", "uwubigbrain.club"],
+    })
+);
 
-app.set('views', `${__dirname}/app`);
-app.use('/css', express.static(`./app/resources/css`));
-app.use('/fonts', express.static(`./app/resources/fonts`));
-app.use('/favicon.ico', express.static(`./app/resources/favicon/favicon.ico`));
+app.set("views", `${__dirname}/app`);
+app.use("/css", express.static(`./app/resources/css`));
+app.use("/fonts", express.static(`./app/resources/fonts`));
+app.use("/favicon.ico", express.static(`./app/resources/favicon/favicon.ico`));
 
 // Configure view caching
 //app.enable('view cache');
 
 //const { exec } = require("child_process");
 
+app.use("/private/*", (request, response, next) => {
+    if (!request.session.user) {
+        response.redirect("/auth/login");
+        return;
+    }
 
-app.use('/private/*', (request, response, next) => {
-  if (!request.session.user) {
-    response.redirect("/auth/login");
+    if (!config.discord.userIDs.includes(request.session.user.id)) {
+        response.status(401).render("error/error", { response });
+        return;
+    }
+
+    //next()
+    response.send("<h1> Marta Blažková </h1>");
     return;
-  }
+});
 
-  if (!config.userIDs.includes(request.session.user.id)) {
-    response.status(401).render('error/error', { response });
-    return;
-  }
+app.get("/", (request, response) => {
+    response.render("main/main", { request });
+});
 
-  //next()
-  response.send("<h1> Marta Blažková </h1>")
-  return;
-})
+app.use("/auth", DiscordAuthRouter);
 
-
-app.get('/', (request, response) => {
-  response.render('main/main', { request });
-})
-
-app.use('/auth', DiscordAuthRouter)
-
-app.get('/about', (request, response) => {
-  response.render('about/about', { request, response })
-})
+app.get("/about", (request, response) => {
+    response.render("about/about", { request, response });
+});
 
 app.use((request, response, next) => {
-  response.status(404).render('error/error', { response, request });
-})
+    response.status(404).render("error/error", { response, request });
+});
 
 app.use((error, request, response, next) => {
-  response.status(500).render('error/error', { response, request })
-})
+    response.status(500).render("error/error", { response, request });
+});
 
+if (!config.server.IsRunningLocally) {
+    const credentials = {};
+    const privateKey = fs.readFileSync(
+        "/etc/letsencrypt/live/uwubigbrain.club/privkey.pem",
+        "utf8"
+    );
+    const certificate = fs.readFileSync(
+        "/etc/letsencrypt/live/uwubigbrain.club/fullchain.pem",
+        "utf8"
+    );
 
+    credentials.key = privateKey;
+    credentials.cert = certificate;
 
-if (!config.IsRunningLocally) {
+    var httpServer = http.createServer(app);
+    var httpsServer = https.createServer(credentials, app);
 
-  const credentials = {};
-  const privateKey = fs.readFileSync('/etc/letsencrypt/live/uwubigbrain.club/privkey.pem', 'utf8');
-  const certificate = fs.readFileSync('/etc/letsencrypt/live/uwubigbrain.club/fullchain.pem', 'utf8');
-
-  credentials.key = privateKey;
-  credentials.cert = certificate;
-
-  var httpServer = http.createServer(app);
-  var httpsServer = https.createServer(credentials, app);
-
-  httpServer.listen(process.env.PORT || 80, () => { console.log("Listening on port 80") });
-  httpsServer.listen(process.env.PORT || 443, () => { console.log("Listening on port 443") });
-
-}
-else {
-
-  app.listen(process.env.PORT || 8080, () => { console.log("Running on localhost:8080") });
-
+    httpServer.listen(process.env.PORT || 80, () => {
+        console.log("Listening on port 80");
+    });
+    httpsServer.listen(process.env.PORT || 443, () => {
+        console.log("Listening on port 443");
+    });
+} else {
+    app.listen(process.env.PORT || 8080, () => {
+        console.log("Running on localhost:8080");
+    });
 }
